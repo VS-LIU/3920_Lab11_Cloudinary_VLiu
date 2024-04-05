58b02fa7105fd7bd19eecdc43cbfb544c64727fb
VS-LIU <vctl95@gmail.com>
2024-04-05 15:34:07 -0700

    Add feature to delete user image

--- routes/router.js
+++ routes/router.js
@@ -107,7 +107,7 @@ router.post('/setUserPic', upload.single('image'), function(req, res, next) {
 					user_id: Joi.string().alphanum().min(24).max(24).required()
 				});
 			
-				const validationResult = schema.validate({pet_id, user_id});
+				const validationResult = schema.validate({user_id});
 				if (validationResult.error != null) {
 					console.log(validationResult.error);
 
@@ -273,6 +273,49 @@ router.get('/deleteUser', async (req, res) => {
 	catch(ex) {
 		res.render('error', {message: 'Error connecting to MongoDB'});
 		console.log("Error connecting to MongoDB");
+		console.log(ex);	
+	}
+});
+
+router.get('/deleteUserImage', async (req, res) => {
+	try {
+		console.log("delete pet image");
+
+		let user_id = req.query.user;
+
+		const schema = Joi.object(
+			{
+				user_id: Joi.string().alphanum().min(24).max(24).required(),
+			});
+		
+		const validationResult = schema.validate({user_id});
+		
+		if (validationResult.error != null) {
+			console.log(validationResult.error);
+
+			res.render('error', {message: 'Invalid user_id'});
+			return;
+		}				
+
+		if (user_id) {
+			console.log("user_id: "+user_id);
+			const success = await userCollection.updateOne({"_id": new ObjectId(user_id)},
+				{$set: {image_id: undefined}},
+				{}
+			);
+
+			console.log("delete User Image: ");
+			console.log(success);
+			if (!success) {
+				res.render('error', {message: 'Error connecting to MySQL'});
+				return;
+			}
+		}
+		res.redirect(`/`);
+	}
+	catch(ex) {
+		res.render('error', {message: 'Error connecting to MySQL'});
+		console.log("Error connecting to MySQL");
 		console.log(ex);	
 	}
 });
